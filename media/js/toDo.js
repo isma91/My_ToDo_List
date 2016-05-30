105aa933eccc1c69f5f6907b95c55f7c0a1d22b8
isma91 <ismaydogmus@gmail.com>
2016-05-30 16:51:30 +0200

    jslint

--- media/js/toDo.js
+++ media/js/toDo.js
@@ -2,7 +2,9 @@
 /*jslint devel : true*/
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
-    var valeurAjoutTitre, valeurAjout, compteur, validate_panel_title, validate_panel_body, panel_to_delete;
+    var valeurAjoutTitre;
+    var valeurAjout;
+    var compteur;
     compteur = 0;
     if (navigator.onLine === false) {
         alert("Vous n'avez pas de connexion internet !!");
@@ -22,14 +24,19 @@ document.addEventListener("DOMContentLoaded", function () {
 /*
 * We create this function here to get scoped in the html file
 */
-function validate_panel (id_panel) {
-    validate_panel_title = document.getElementById('panel_title_' + id_panel).innerHTML
-    validate_panel_body = document.getElementById('panel_body_' + id_panel).innerHTML
+var validate_panel_title;
+var validate_panel_body;
+var panel_to_delete;
+function remove_panel(id_panel) {
+    "use strict";
+    panel_to_delete = document.getElementById("panel_" + id_panel);
+    panel_to_delete.remove();
+}
+function validate_panel(id_panel) {
+    "use strict";
+    validate_panel_title = document.getElementById('panel_title_' + id_panel).innerHTML;
+    validate_panel_body = document.getElementById('panel_body_' + id_panel).innerHTML;
     validate_panel_body = validate_panel_body.replace('<img class="validate" src="media/icons/validate.png" alt="validation" onclick="validate_panel(' + id_panel + ')">', '');
     remove_panel(id_panel);
     document.getElementById('fini').innerHTML = document.getElementById('fini').innerHTML + '<div class="panel panel-default" id="panel_' + id_panel + '"><div class="panel-heading"><h3 class="panel-title">' + validate_panel_title + '</h3><img class="delete" src="media/icons/delete.png" alt="delete" onclick="remove_panel(' + id_panel + ')" /></div><div class="panel-body">' + validate_panel_body + '</div></div>';
-}
-function remove_panel (id_panel) {
-    panel_to_delete = document.getElementById("panel_" + id_panel);
-    panel_to_delete.remove();
 }
